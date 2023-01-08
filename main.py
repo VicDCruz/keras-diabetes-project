f28ff4d14d8cb86b2509e6bf95c4989bfbba4f46
Victor <vicdan1@comunidad.unam.mx>
2023-01-07 19:53:56 -0600

    Make predictions with model

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def load_dataset():
     return dataset[:, 0:8], dataset[:, 8]
 
 
-def build_model():
+def build_model() -> tf.keras.models.Sequential:
     model = tf.keras.models.Sequential()
     model.add(tf.keras.layers.Dense(24, input_shape=(8,), activation='relu'))
     model.add(tf.keras.layers.Dropout(0.2))
@@ -15,6 +15,10 @@ def build_model():
     model.add(tf.keras.layers.Dropout(0.2))
     model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
     return model
+
+
+def predict(model: tf.keras.models.Sequential, X):
+    return (model.predict(X) > 0.5).astype(int)
 
 
 if __name__ == '__main__':
@@ -25,3 +29,7 @@ if __name__ == '__main__':
     model.fit(X, y, epochs=150, batch_size=10)
     _, accuracy = model.evaluate(X, y)
     print('Accuracy: %.2f' % (accuracy * 100))
+
+    predictions = predict(model, X)
+    for i in range(5):
+        print('%s => %d (expected %d)' % (X[i].tolist(), predictions[i], y[i]))
